0256fab2bd4db79c6c3b4e6b94475ee22ba27989
Artem Korolchuk <artemkorolchukua@gmail.com>
2025-10-29 10:37:18 +0100

    Practise Composition. Smart kitchen challenge: modeling IoT appliances with composition

---
+++ OOP/Composition/CompositionChallenge/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        SmartKitchen smartKitchen = new SmartKitchen();
+
+        smartKitchen.setKitchenState(true, false, true);
+        smartKitchen.doKitchenWork();
+    }
+}
---
+++ OOP/Composition/CompositionChallenge/src/SmartKitchen.java
@@ -0,0 +1,97 @@
+public class SmartKitchen {
+
+    private CoffeeMaker brewMaster;
+    private DishWasher dishWasher;
+    private Refrigerator iceBox;
+
+    public SmartKitchen() {
+        brewMaster = new CoffeeMaker();
+        dishWasher = new DishWasher();
+        iceBox = new Refrigerator();
+    }
+
+    public CoffeeMaker getBrewMaster() {
+        return brewMaster;
+    }
+
+    public DishWasher getDishWasher() {
+        return dishWasher;
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+
+    private void addWater() {
+        brewMaster.brewCoffee();
+    }
+
+    private void pourMilk() {
+        iceBox.orderFood();
+    }
+
+    private void loadDishWasher() {
+        dishWasher.doDishes();
+    }
+
+    public void setKitchenState(boolean coffeeFlag, boolean fridgeFlag, boolean dishWasherFlag) {
+        brewMaster.setHasWorkToDo(coffeeFlag);
+        iceBox.setHasWorkToDo(fridgeFlag);
+        dishWasher.setHasWorkToDo(dishWasherFlag);
+    }
+
+    public void doKitchenWork() {
+
+        addWater();
+        pourMilk();
+        loadDishWasher();
+    }
+}
+
+
+class CoffeeMaker {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void brewCoffee() {
+        if (hasWorkToDo) {
+            System.out.println("CoffeeMaker brews coffee");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+class Refrigerator {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void orderFood() {
+        if (hasWorkToDo) {
+            System.out.println("Refrigerator orders food");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+class DishWasher {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void doDishes() {
+        if (hasWorkToDo) {
+            System.out.println("DishWasher does dishes");
+        }
+    }
+}
