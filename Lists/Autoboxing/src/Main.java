bcf2dcbf0d625e64edd3eaa32b2e59a599386c17
Artem Korolchuk <artemkorolchukua@gmail.com>
2025-11-12 16:16:37 +0100

    Autoboxing & Unboxing for Arrays & Lists (Wrapper Classes)

---
+++ Lists/Autoboxing/src/Main.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Integer boxedInt = Integer.valueOf(15);
+        int unboxingInt = boxedInt.intValue();
+
+        // Automatic
+        Integer autoBoxed = 15;
+        int autoUnboxed = autoBoxed;
+        System.out.println(autoBoxed.getClass().getName());
+//        System.out.println(autoUnboxed.getClass().getName());
+
+        Double resultBoxed = getLiteralDoublePrimitive();
+        double resultUnboxed = getDoubleObject();
+
+        Integer[] wrappedArray = new Integer[5];
+        wrappedArray[0] = 50;
+        System.out.println(Arrays.toString(wrappedArray));
+
+        System.out.println(wrappedArray[0].getClass().getName());
+
+        Character[] characterArray = {'a', 'b', 'c', 'd'};
+        System.out.println(Arrays.toString(characterArray));
+
+        var outList = getList(1, 2, 3, 4, 5);
+        System.out.println(outList);
+
+    }
+
+    private static ArrayList<Integer> getList(int... varargs) {
+        ArrayList<Integer> aList = new ArrayList<>();
+        for (int i : varargs) {
+            aList.add(i);
+        }
+        return aList;
+    }
+
+    private static int returnAnInt(Integer i) {
+        return i;
+    }
+
+    private static Integer returnAnInteger(int i) {
+        return i;
+    }
+
+    private static Double getDoubleObject() {
+
+        return Double.valueOf(100.0);
+    }
+
+    private static double getLiteralDoublePrimitive() {
+
+        return 100.0;
+    }
+}
